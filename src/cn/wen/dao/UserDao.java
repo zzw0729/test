aba3935e5bd78e841e705cb842738bff81df439c
zzw0729 <1779684450@qq.com>
2019-09-04 11:04:25 +0800

    di第一次从idea上提交

---
+++ src/cn/wen/dao/UserDao.java
@@ -0,0 +1,12 @@
+package cn.wen.dao;
+
+/**
+ * @author: zhangzhiwen
+ * @date: 2019-09-04 10:43
+ */
+
+public class UserDao {
+    public static void main(String[] args) {
+        System.out.println("ggg");
+    }
+}

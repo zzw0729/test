c16ff1c01bf2dbe3f2f3247ece8a92522be0a741
HDhaha <1779684450@qq.com>
2019-09-05 11:37:38 +0800

    wen哥提交

---
+++ src/cn/wen/dao/ASs.java
@@ -0,0 +1,12 @@
+package cn.wen.dao;
+
+/**
+ * @author: zhangzhiwen
+ * @date: 2019-09-05 11:37
+ */
+
+public class ASs {
+    public static void main(String[] args) {
+        System.out.println("ff");
+    }
+}
